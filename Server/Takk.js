312500b2f4365e4359f6b8898149dbdf97e02d04
Takk <takkxiiv@gmail.com>
2024-10-23 16:40:35 +0800

    commit-Takk

--- Server/Takk.js
+++ Server/Takk.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.listen(port, () => {console.log('App is running on localhost:8000')})
+app.listen(port, () => {console.log('Server is running on localhost:8000')})
