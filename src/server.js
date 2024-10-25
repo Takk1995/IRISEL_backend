44edc4f2d616dd677c78f4719aabd273bc699137
Takk <takkxiiv@gmail.com>
2024-10-25 16:02:48 +0800

    commit-Takk

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8000;
-const adminRoutes = require('./routes/adminApi')
+const adminRoutes = require('./routes/adminApi');
+const cartRoutes = require('./routes/cartApi')
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', adminRoutes);
+app.use('/api', cartRoutes);
 
 
 
