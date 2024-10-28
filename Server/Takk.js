1e3960dab6e389ecb97a58e26464a0d8a330bde4
KiKimon-Bow <kkiikk5959@gmail.com>
2024-10-28 15:14:40 +0800

    commit-Yuka

--- Server/Takk.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cors = require('cors')
-const IRISEL = express();
-const port = 8000;
-
-
-
-IRISEL.listen(port, () => {console.log('Server is running on localhost:8000')})
