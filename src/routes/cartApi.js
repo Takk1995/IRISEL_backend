83fa1b34509ffbd65623e94daf62ca3c4be11f5f
takk <takkxiiv@gmail.com>
2024-10-29 23:49:18 +0800

    commit-Takk

--- src/routes/cartApi.js
+++ src/routes/cartApi.js
@@ -20,4 +20,26 @@ router.post('/cartItem', async(req, res) => {
     }
 })
 
+router.get('/updateCapacity', async(req, res) => {
+    const {product_code} = req.query
+        
+    if (!product_code) {
+        return res.status(400).json({error: 'error'})
+    }
+    const numericProductCode = Number(product_code)
+    console.log(numericProductCode);
+    
+    try {
+        const [result] = await pool.query('SELECT * FROM products WHERE product_code = ?', [product_code])
+        if (result.length === 0) {
+            return res.status(400).json({error: 'error'})
+        }
+        console.log(result[0]);
+        
+        res.json(result[0])
+    } catch (error) {
+        res.status(400).json({error: 'error'})
+    }
+})
+
 module.exports = router;
